e3d4f01c7e49a71d83c54cff27828c4cea00775f
izaan.noman <izaan.nyc@gmail.com>
2023-11-19 00:31:09 -0500

    Change host and port

--- app.py
+++ app.py
@@ -199,4 +199,4 @@ def process_and_add_assignments(calendar_data):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        app.run(debug=True)
+        app.run(debug=True, host="0.0.0.0", port=80)
